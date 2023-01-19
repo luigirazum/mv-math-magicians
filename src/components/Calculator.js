593bc8ee3feb4e1cd18dfc57754d8a59181728c0
Luis Zubia <soyzubia@gmail.com>
2023-01-19 16:39:31 -0600

    Calculator component refactor to Hooks.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,54 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import KeyButton from './KeyButton';
 import Display from './Display';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '0',
-      next: null,
-    };
+const Calculator = (props) => {
+  const [calcOptions, setCalcOptions] = useState({ total: '0', next: null });
 
-    this.handlePressKeyButton = this.handlePressKeyButton.bind(this);
-  }
+  const handlePressKeyButton = (e) => {
+    console.log(calcOptions);
+    const newCalcOptions = calculate(calcOptions, e.target.name);
+    setCalcOptions(newCalcOptions);
+    console.log(calcOptions);
+  };
 
-  handlePressKeyButton(e) {
-    const currentState = this.state;
-    const newState = calculate(currentState, e.target.name);
-    this.setState(newState);
-  }
+  const { calcKeys, calcKeysNames, title } = props;
+  const calcBtnClasses = calcKeysNames.split(',');
+  const calcButtons = calcKeys.split(',')
+    .map((calcKey, index) => {
+      const keyName = 'id'.concat(index.toString());
+      const classKey = index < 10 ? 'num'.concat(calcKey.toString()) : calcBtnClasses[index - 10];
+      return (
+        <KeyButton
+          key={keyName}
+          keyTag={calcKey}
+          keyClass={classKey}
+          onPressKey={handlePressKeyButton}
+        />
+      );
+    });
 
-  render() {
-    const { calcKeys, calcKeysNames, title } = this.props;
-    const calcBtnClasses = calcKeysNames.split(',');
-    const calcButtons = calcKeys.split(',')
-      .map((calcKey, index) => {
-        const keyName = 'id'.concat(index.toString());
-        const classKey = index < 10 ? 'num'.concat(calcKey.toString()) : calcBtnClasses[index - 10];
-        return (
-          <KeyButton
-            key={keyName}
-            keyTag={calcKey}
-            keyClass={classKey}
-            onPressKey={this.handlePressKeyButton}
-          />
-        );
-      });
-
-    const { total, next } = this.state;
-    const value = !next ? total : next;
-    return (
-      <div title={title} className="Calculator">
-        <Display result={value} />
-        {calcButtons}
-      </div>
-    );
-  }
-}
+  const { total, next } = calcOptions;
+  const value = !next ? total : next;
+  return (
+    <div title={title} className="Calculator">
+      <Display result={value} />
+      {calcButtons}
+    </div>
+  );
+};
 
 Calculator.defaultProps = {
   calcKeys: '0,1,2,3,4,5,6,7,8,9,.,AC,+/-,%,÷,x,-,+,=',
