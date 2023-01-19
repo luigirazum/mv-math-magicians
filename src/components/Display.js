ef16e43beda584731cfaf06904819e499104ce9d
Luis Zubia <soyzubia@gmail.com>
2023-01-19 14:56:26 -0600

    Display component used by Calculator refactor to Hooks.

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Display extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-  }
-
-  render() {
-    const { result } = this.props;
-    return (
-      <div className="display"><p>{result || '0'}</p></div>
-    );
-  }
-}
+const Display = (props) => {
+  const { result } = props;
+  return (
+    <div className="display"><p>{result || '0'}</p></div>
+  );
+};
 
 Display.propTypes = {
   result: PropTypes.string,
