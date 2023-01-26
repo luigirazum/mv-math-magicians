84c98159bbe29117ad29ce012794d794e930b7ba
Luis Zubia <soyzubia@gmail.com>
2023-01-25 23:06:15 -0600

    Add NotMatch page, it shows when a no valid address is entered in the browser.

---
+++ src/pages/NotMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotMatch = () => (
+  <div>
+    <h3>
+      Sorry!! You misscalculate, the page doesn&apos;t exist.
+    </h3>
+  </div>
+);
+
+export default NotMatch;
