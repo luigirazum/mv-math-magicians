be4f91960f2d2daeba0bf7001589caaab7309879
Luis Zubia <soyzubia@gmail.com>
2023-01-26 00:17:14 -0600

    Create a PageHeader component that is composed for a h1 title and the NavBar component.

---
+++ src/components/PageHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Add the NavBar to the Header
+import NavBar from './NavBar';
+
+const PageHeader = () => (
+  <div>
+    <header>
+      <h1>Math Magicians</h1>
+      <NavBar />
+    </header>
+  </div>
+);
+
+export default PageHeader;
