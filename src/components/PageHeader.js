87366438c1fe6463792da87cf295f13f99e00555
Luis Zubia <soyzubia@gmail.com>
2023-01-26 11:36:54 -0600

    Add styles to the header component.

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+// import styles module
+import phstyles from './PageHeader.module.css';
+
 // Add the NavBar to the Header
 import NavBar from './NavBar';
 
 const PageHeader = () => (
   <div>
-    <header>
-      <h1>Math Magicians</h1>
+    <header className={phstyles.header}>
+      <h1 className={phstyles.h1}>Math Magicians</h1>
       <NavBar />
     </header>
   </div>
