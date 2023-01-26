e8138a05ecad6b8f070cf6be12d5d45eb9733351
Luis Zubia <soyzubia@gmail.com>
2023-01-26 10:37:54 -0600

    Add LoremIpsum component to the HomePage.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import LoremIpsum from 'react-lorem-ipsum';
 
-const HomePage = () => (<div><h2>This is the HomePage</h2></div>);
+const HomePage = () => (
+  <div>
+    <h2>This is the HomePage</h2>
+    <LoremIpsum
+      p={2}
+      avgSentencesPerParagraph={4}
+      random={false}
+    />
+  </div>
+);
 
 export default HomePage;
